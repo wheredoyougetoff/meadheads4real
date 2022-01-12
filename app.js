a0ec5fd6369cd3f4b8166fe89a724962aba5f075
Dylan Massey <massey.dylan00@gmail.com>
2022-01-12 00:18:24 -0600

    pranking ryan, gottem

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 let intro = document.querySelector(".intro");
 let logo = document.querySelector(".logo-header");
 let logoSpan = document.querySelectorAll(".logo");
+let logoPic = document.querySelectorAll(".logoPic");
 
 window.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
     logoSpan.forEach((span, idx) => {
-      setTimeout(
-        () => {
-          span.classList.add("active");
-        },
-        (idx + 1) * 600,
-        (idx + 2) * 600
-      );
+      setTimeout(() => {
+        span.classList.add("active");
+      }, (idx + 1) * 600);
     });
 
     setTimeout(() => {
@@ -23,8 +20,24 @@ window.addEventListener("DOMContentLoaded", () => {
       });
     }, 2000);
 
-    // setTimeout(() => {
-    //   intro.style.top = "-100vh";
-    // }, 2300);
+    setTimeout(() => {
+      logoPic.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.add("active");
+        }, (idx + 0) * 1000);
+      });
+    });
+    setTimeout(() => {
+      logoPic.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.remove("active");
+          span.classList.add("fade");
+        });
+      });
+    }, 2000);
+
+    setTimeout(() => {
+      intro.style.top = "-100vh";
+    }, 2300);
   });
 });
